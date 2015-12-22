abe7f533b30904f972b00e0381668d571aeb325e
Cole Brumley <cole.brumley@gmail.com>
2015-12-21 22:21:31 -0600

    ...

--- convoy/cfg/convoy.sh
+++ convoy/cfg/convoy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e; set -x
 mkdir -p /opt/lib/convoy/volumes /opt/bin /var/{lib,run}/convoy /etc/docker/plugins || true
-rm -f /convoy/SHA* ; mv /convoy/* /opt/bin/
+mv /convoy/* /opt/bin/ || true
 echo "unix:///var/run/convoy/convoy.sock" > /etc/docker/plugins/convoy.spec
 /opt/bin/convoy daemon --drivers vfs --driver-opts vfs.path=/opt/lib/convoy/volumes
