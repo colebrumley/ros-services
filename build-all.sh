ab05e0becee726446ef4f456ae019a73d26fbca3
Cole Brumley <cole.brumley@gmail.com>
2015-12-21 23:53:46 -0600

    overscript!

--- build-all.sh
+++ build-all.sh
@@ -8,16 +8,13 @@ REPO_TAG=${REPO_TAG:-latest}
 export REPO_USER REPO_TAG
 cd $(dirname $0)
 
-for d in $(ls -d */); do
-	if [[ -x $d/build.sh ]]; then
-		./$d/build.sh
-	fi
-done
-
 set +x
 docker login -e $DOCKER_EMAIL -u $DOCKER_REPO_LOGIN -p $DOCKER_PASS
 set -x
 
-if [[ "${1}" == "push" ]]; then
-	for d in $(ls -d */); do docker push $REPO_USER/os-$(echo ${d} | sed 's/\/$//'):${REPO_TAG}; done
-fi
+for d in $(ls -d */); do
+	if [[ -x $d/build.sh ]]; then
+		./$d/build.sh $1
+	fi
+done
+
--- convoy/build.sh
+++ convoy/build.sh
@@ -12,7 +12,9 @@ wget $convoy_url
 tar xzvf convoy.tar.gz
 
 for f in $(ls *Dockerfile); do
-	docker build -t ${REPO_USER:-elcolio}/os-${PWD##*/}-$(echo $f | sed 's/\.Dockerfile//'):${REPO_TAG:-latest} -f $f .
+	cn=${REPO_USER:-elcolio}/os-${PWD##*/}-$(echo $f | sed 's/\.Dockerfile//'):${REPO_TAG:-latest}
+	docker build -t $cn -f $f .
+	if [[ "$1" == "push" ]]; then docker push $cn; fi
 done
 
 rm -Rf convoy/ convoy.tar.gz
